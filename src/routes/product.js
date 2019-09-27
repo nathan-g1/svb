60094b8b75d7ae79e64d8e304557e1202a767e16
Nathan Getachew <nathygech111@gmail.com>
2019-09-27 16:58:02 +0300

    add and update product
    - check user type and perform update on product

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../model/Product');
+const User = require('../model/User');
 
 router.get('/', async (req, res) => {
     // res.send('these are all the products');
@@ -19,7 +20,7 @@ router.get('/:id', async (req, res) => {
     console.log('request to the product: ' + req.params.id);
     try {
         const products = await Product.findById(req.params.id);
-        res.json(products);
+        return res.json(products);
     } catch (err) {
         res.send({ message: err });
     }
@@ -28,15 +29,51 @@ router.get('/:id', async (req, res) => {
 
 router.post('/add', async (req, res) => {
     const product = new Product({
-        name: req.body.name,
-        price: req.body.price,
+        name: req.body.product.name,
+        price: req.body.product.price,
+        description: req.body.product.description,
+        model: req.body.product.model
+    });
+    const user = new User({
+        id: req.body.user.id,
+        name: req.body.user.name,
+        email: req.body.user.email,
+        type: req.body.user.type
     });
     try {
-        const newProduct = await product.save();
-        res.json(newProduct);
+        console.log(req.body);
+        if (user.type === "adm") {
+            const newProduct = await product.save();
+            return res.json(newProduct);
+        } else {
+            return res.json({ message: "Not allowed to add for this user!" });
+        }
     } catch (err) {
-        res.json({ message: err });
+        return res.json({ message: err });
     }
 });
+
+router.put('/update/:id', async (req, res) => {
+    const user = new User({
+        id: req.body.user.id,
+        name: req.body.user.name,
+        email: req.body.user.email,
+        type: req.body.user.type
+    });
+    try {
+        if (user.type === "adm") {
+            await Product.findByIdAndUpdate({ _id: req.params.id }, req.body.product);
+            const _afterProduct = await Product.findOne({ _id: req.params.id });
+            return res.json(_afterProduct);
+        } else {
+            return res.json({ message: "Not allowed to update for this user!" });
+        }
+    } catch (err) {
+        return res.json({ message: err });
+    }
+});
+
+
+
 
 module.exports = router;
