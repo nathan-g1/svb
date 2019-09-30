adf86a6b23d4aff04bf7939ed3d86f2ccbfda051
Nathan Getachew <nathygech111@gmail.com>
2019-09-30 10:08:47 +0300

    update Order model
    add new properties
    - description
    - productId &
    - userId

--- src/model/Order.js
+++ src/model/Order.js
@@ -2,9 +2,22 @@ const mongoose = require('mongoose');
 const OrderSchema = mongoose.Schema({
    //requres product id and user id 
    quantity: {
-      type: number,
+      type: Number,
       required: true
+   },
+   productId: {
+      type: String,
+      require: true
+   },
+   userId: {
+      type: String,
+      require: true
+   },
+   description: {
+      type: String,
+      require: false
    }
+
 });
 
 module.exports = mongoose.model('Orders', OrderSchema);
