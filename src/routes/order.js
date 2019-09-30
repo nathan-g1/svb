1f305b833343594b8c3a77d51a47d895da1ccf2a
Nathan Getachew <nathygech111@gmail.com>
2019-09-30 16:42:01 +0300

    check type of user before sending order list

--- src/routes/order.js
+++ src/routes/order.js
@@ -4,8 +4,12 @@ const Order = require('../model/Order');
 
 router.get('/', async (req, res) => {
     // res.send('these are all the products');
+    // requires the front-end to send the type of the user
     console.log('request to all the orders');
     try {
+        if (req.body.user.type !== 'adm') {
+            return res.json({ message: 'opperation not allowd for this user' });
+        }
         const products = await Order.find();
         return res.json(products);
     } catch (err) {
@@ -14,6 +18,10 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
+    // requires the front-end to send the type of the user
+    if (req.body.user.type !== 'adm') {
+        return res.json({ message: 'opperation not allowd for this user' });
+    }
     console.log('request to all the orders');
     try {
         const singleProduct = await Order.findOne({ _id: req.params.id });
