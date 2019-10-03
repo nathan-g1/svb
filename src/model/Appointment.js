f90a36fd3d1b6bfdee67e387b51beeb9b2f7c2ab
Nathan Getachew <nathygech111@gmail.com>
2019-10-03 13:11:46 +0300

    appointment schema with main properties

--- src/model/Appointment.js
+++ src/model/Appointment.js
@@ -1,1 +1,36 @@
 
+const mongoose = require('mongoose');
+const AppointmentSchema = mongoose.Schema({
+    // requires physicians id(user) and time(date & hour range)
+    physicianId: {
+        type: String,
+        require: true
+    },
+    startingHour: {
+        type: String,
+        require: true
+    },
+    finishingHour: {
+        type: String,
+        require: true
+    },
+    note: {
+        type: String,
+        require: false
+    },
+    patientId: {
+        type: String,
+        require: false
+    },
+    selectedHour: {
+        type: Date,
+        require: false
+    },
+    booked: {
+        type: Boolean,
+        require: true,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('Appointment', AppointmentSchema);
