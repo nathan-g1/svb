1784b1c0eb82f800861fb45e7ea6d3cd60c3bb06
Nathan Getachew <nathygech111@gmail.com>
2019-10-05 15:18:59 +0300

    enhance product route
    - add new product with images
    - support multiple images
    - send the right message for successful update and deletion

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../model/Product');
-const User = require('../model/User');
+const upload = require('../middleware/uploadFile');
 
 router.get('/', async (req, res) => {
     // res.send('these are all the products');
@@ -15,7 +15,6 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-    // res.send(req.params.id);
     // check/handle the id coming from the user and pass to the product
     console.log('request to the product: ' + req.params.id);
     try {
@@ -26,28 +25,25 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-
-router.post('/add', async (req, res) => {
+// add new product with image
+// http expects multi-form data instead of raw body data from the request
+router.post('/add', upload.array('productImages', 5), async (req, res) => {
+    console.log(req.files);
+    const imagePath = [];
+    for (let i = 0; i < req.files.length; i++) {
+        imagePath.push(req.files[i].path);
+    }
     const product = new Product({
-        name: req.body.product.name,
-        price: req.body.product.price,
-        description: req.body.product.description,
-        model: req.body.product.model
-    });
-    const user = new User({
-        id: req.body.user.id,
-        name: req.body.user.name,
-        email: req.body.user.email,
-        type: req.body.user.type
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+        model: req.body.model,
+        type: req.body.type,
+        image: imagePath
     });
     try {
-        console.log(req.body);
-        if (user.type === "adm") {
-            const newProduct = await product.save();
-            return res.json(newProduct);
-        } else {
-            return res.json({ message: "Not allowed to add for this user!" });
-        }
+        const newProduct = await product.save();
+        return res.json({ message: "successfully added a new Product", newProduct });
     } catch (err) {
         return res.json({ message: err });
     }
@@ -58,7 +54,7 @@ router.put('/update/:id', async (req, res) => {
         if (req.body.user.type === "adm") {
             await Product.findByIdAndUpdate({ _id: req.params.id }, req.body.product);
             const _afterProduct = await Product.findOne({ _id: req.params.id });
-            return res.json(_afterProduct);
+            return res.json({ message: "successfully updated", product: _afterProduct });
         } else {
             return res.json({ message: "Not allowed to update for this user!" });
         }
@@ -72,7 +68,7 @@ router.delete('/delete/:id', async (req, res) => {
         if (req.body.user.type === "adm") {
             const showPdt = await Product.findById({ _id: req.params.id });
             await Product.findByIdAndDelete({ _id: req.params.id });
-            return res.json(showPdt);
+            return res.json({ message: "successfully deleted", product: showPdt });
         } else {
             return res.json({ message: "Opperation Not allowed for this user!" });
         }
