e7b83be4f701c3b4e4ed9519766a398090b0069c
Nathan Getachew <nathygech111@gmail.com>
2019-10-05 15:16:38 +0300

    configure image upload using multer

---
+++ src/middleware/uploadFile.js
@@ -0,0 +1,31 @@
+const multer = require('multer');
+const path = require('path');
+
+const fileFilter = (req, file, cb) => {
+  // reject files that are not jpeg, png, bmp, GIF
+  var ext = path.extname(file.originalname);
+  if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg' && ext !== '.bmp') {
+    return callback(new Error('Only images are allowed'));
+  }
+  cb(null, true);
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './images/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + '_' + file.originalname);
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 8 * 1024 * 1024,
+  },
+  fileFilter: fileFilter
+});
+
+
+module.exports = upload
