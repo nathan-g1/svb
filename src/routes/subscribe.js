2f7eca297563f62dab4db252368e4e6d8492bc55
Nathan Getachew <nathygech111@gmail.com>
2019-10-15 16:45:25 +0300

    fix: add try catch to subscribe route

--- src/routes/subscribe.js
+++ src/routes/subscribe.js
@@ -5,20 +5,28 @@ const Subscription = require('../model/Subscription');
 
 // get all subscibed emails
 router.get('/', async (req, res) => {
-    const emails = await Subscription.find();
-    return res.json({ message: "All emails on subcription list", emails });
+    try {
+        const emails = await Subscription.find();
+        return res.json({ message: "All emails on subcription list", emails });
+    } catch (err) {
+        return res.status(400).json(err);
+    }
 });
 
 // subscirbe for news letters and update
 router.post('/add', async (req, res) => {
-    if (validator.isEmail(req.body.email)) {
-        const subsc = new Subscription({
-            email: req.body.email
-        });
-        await subsc.save();
-        return res.json({ message: subsc.email + " saved for subscription" });
-    } else {
-        return res.json({ message: "invalid email" });
+    try {
+        if (validator.isEmail(req.body.email)) {
+            const subsc = new Subscription({
+                email: req.body.email
+            });
+            await subsc.save();
+            return res.json({ message: subsc.email + " saved for subscription" });
+        } else {
+            return res.json({ message: "invalid email" });
+        }
+    } catch (err) {
+        return res.status(400).json(err);
     }
 });
 module.exports = router;
